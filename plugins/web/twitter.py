a345edf823505820060b7336dda4b216ae5cee67
nathan0 <mail@cuddlerape.me>
2015-05-08 00:48:41 +0100

    updated twitter plugin, added colors for @mentions and #hashtags

--- plugins/web/twitter.py
+++ plugins/web/twitter.py
@@ -3,10 +3,14 @@ from lxml import html
 from bot.events import Callback, msghandler
 from util.irc import Message
 
-class Twitter(Callback):
+hcolor = "06"
+mcolor = "11"
+r = re.compile(r"^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)")
+n = re.compile(r"<.*?>",re.DOTALL)
+m = re.compile("@(.*?)\)?\s")
+h = re.compile("#(.*?)\s")
 
-    r = re.compile("^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)")
-    n = re.compile(r"<.*?>",re.DOTALL)
+class Twitter(Callback):
 
     def __init__(self, server):
         self.server = server
@@ -15,10 +19,17 @@ class Twitter(Callback):
     @Callback.inline
     @msghandler
     def trigger(self, server, line):
-        link = self.r.search(line.text)
+        link = r.search(line.text)
         if link:
             tweet = requests.get("https://api.twitter.com/1/statuses/oembed.json",params={"id":link.group(2)})
             if tweet.ok:
-                return "\x0308│\x03 {}".format(html.fromstring(self.n.sub("",tweet.json()["html"])).text)
+                tweet = html.fromstring(n.sub("",tweet.json()["html"])).text.split("—")[0]
+                mentions = m.findall(tweet)
+                hashtags = h.findall(tweet)
+                for _m in mentions:
+                    tweet = tweet.replace("@{}".format(_m),"\x03{}@{}\x03".format(mcolor,_m))
+                for _h in hashtags:
+                    tweet = tweet.replace("#{}".format(_h),"\x03{}#\x02\x02{}\x03".format(hcolor,_h))
+                return "\x0308Tweet│\x03 {}".format(tweet)
 
 __initialise__ = Twitter
