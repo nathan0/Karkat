30d0f5fb9eb55d42907f05952e14cd22b7d966b6
nathan0 <mail@cuddlerape.me>
2015-06-10 19:54:08 +0100

    replace newlines with space in tweets

--- plugins/web/twitter.py
+++ plugins/web/twitter.py
@@ -9,6 +9,7 @@ r = re.compile(r"^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)"
 n = re.compile(r"<.*?>",re.DOTALL)
 m = re.compile("@(.*?)\)?\s")
 h = re.compile("#(.*?)\s")
+s = re.compile("\s+")
 
 class Twitter(Callback):
 
@@ -23,13 +24,13 @@ class Twitter(Callback):
         if link:
             tweet = requests.get("https://api.twitter.com/1/statuses/oembed.json",params={"id":link.group(2)})
             if tweet.ok:
-                tweet = html.fromstring(n.sub("",tweet.json()["html"])).text.split("—")[0]
+                tweet = s.sub(" ",html.fromstring(n.sub("",tweet.json()["html"])).text.split("—")[0].replace("\n"," "))
                 mentions = m.findall(tweet)
                 hashtags = h.findall(tweet)
                 for _m in mentions:
                     tweet = tweet.replace("@{}".format(_m),"\x03{}@{}\x03".format(mcolor,_m))
                 for _h in hashtags:
                     tweet = tweet.replace("#{}".format(_h),"\x03{}#\x02\x02{}\x03".format(hcolor,_h))
-                return "\x0300,10Tweet│\x03 {}".format(tweet)
+                return "\x0310│\x03 {}".format(tweet)
 
 __initialise__ = Twitter
